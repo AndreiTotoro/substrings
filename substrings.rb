afa0d95fdd73dbf2d9423ad76e6242feda502dc2
AndreiTotoro <AndreiTotoro@gmail.com>
2022-06-16 15:49:51 +0300

    Clean up the code

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,8 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-#Get word and dictionary
-#Return every possible combination of that word
-#Compare each combination with a word in the dictionary
-#Whenever a string matches store result in a hash
-#Print hash
-
 
 def substrings(word, dictionary) 
   results = Hash.new(0)
-  letter_array = word.split("")
   dictionary.each do |dictionary_word| 
     if(word.downcase.include?(dictionary_word) == true)
       results[dictionary_word] = word.downcase.scan(/#{dictionary_word}/).length
